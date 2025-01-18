5af747b827b2aec9a955258a964316505116e4c4
Amstrad <positivedesign@yandex.ru>
2025-01-18 05:18:15 +0300

    17.01.2024

--- menu.py
+++ menu.py
@@ -9,99 +9,85 @@ from search_notes_function import search_notes
 init(autoreset=True)
 
 
-# Функция выбора номера заметки
+# Функция выбора номера заметки. Запрашивает у пользователя номер
+# заметки и возвращает индекс выбранной заметки.
 def choice_note(notes):
     while True:
         user_input = input(
             "Введите номер заметки (пустой ввод для выхода): "
         ).strip()
-        if not user_input:  # Проверка на пустой ввод
+        if not user_input:
             print("Выход из выбора заметки.")
             return None
         try:
-            note_index = int(user_input) - 1  # Преобразование ввода в индекс
-            if 0 <= note_index < len(notes):  # Проверка корректности индекса
+            note_index = int(user_input) - 1
+            if 0 <= note_index < len(notes):
                 return note_index
-            print("Такой заметки не существует")  # Сообщение об ошибке
+            print("Такой заметки не существует")
         except ValueError:
-            print("Введите число.")  # Обработка некорректного ввода
+            print("Введите число.")
 
 
-# Функция для удаления заметки
+# Функция для удаления заметки. Запрашивает у пользователя номер заметки
+# для удаления. Удаляет заметку, если пользователь подтверждает действие.
 def delete_note(notes):
-    display_notes(notes)  # Отображение всех заметок
-    while True:
-        user_input = input(Fore.YELLOW + "Введите номер заметки для \
-удаления (пустой ввод для выхода): " + Style.RESET_ALL).strip()
-        if not user_input:  # Проверка на пустой ввод
-            print(Fore.YELLOW + "Выход из удаления заметки." + Style.RESET_ALL)
-            return
-        try:
-            note_index = int(user_input) - 1  # Преобразование ввода в индекс
-            if 0 <= note_index < len(notes):  # Проверка корректности индекса
-                confirm = input(Fore.RED + "Вы уверены, что хотите удалить \
-эту заметку? (да/нет): " + Style.RESET_ALL).lower()
-                if confirm == 'да':  # Подтверждение удаления
-                    del notes[note_index]  # Удаление заметки
-                    print(Fore.GREEN + "Заметка удалена!" + Style.RESET_ALL)
-                    return
-                print(Fore.YELLOW + "Удаление отменено." + Style.RESET_ALL)
-                return
-            print(Fore.RED + "Неверный номер \
-заметки." + Style.RESET_ALL)  # Сообщение об ошибке
-        except ValueError:
-            print(Fore.RED + "Ошибка: введите число или оставьте поле пустым \
-для выхода." + Style.RESET_ALL)  # Обработка некорректного ввода
+    display_notes(notes)
+    note_index = choice_note(notes)
+    if note_index is not None:
+        confirm = input(
+            Fore.RED +
+            "Вы уверены, что хотите удалить эту заметку? (да/нет): " +
+            Style.RESET_ALL
+        ).lower()
+        if confirm == 'да':
+            del notes[note_index]
+            print(Fore.GREEN + "Заметка удалена!" + Style.RESET_ALL)
+        else:
+            print(Fore.YELLOW + "Удаление отменено." + Style.RESET_ALL)
 
 
-# Основные функции для меню
+# Основные функции для меню. Создает новую заметку, добавляет её в список
+# и отображает информацию о ней.
 def create_new_note():
-    new_note = create_note()  # Создание новой заметки
-    notes.append(new_note)  # Добавление заметки в список
+    new_note = create_note()
+    notes.append(new_note)
     print(Fore.GREEN + "Заметка создана:" + Style.RESET_ALL)
-    display_note(new_note, len(notes))  # Отображение созданной заметки
+    display_note(new_note, len(notes))
 
 
+# Запрашивает у пользователя номер заметки для обновления.
+# Обновляет выбранную заметку и отображает её информацию.
 def update_existing_note():
-    display_notes(notes)  # Отображение всех заметок
-    if not notes:  # Проверка на наличие заметок
+    display_notes(notes)
+    if not notes:
         print("Нет заметок для изменения.")
         return
-    note_index = choice_note(notes)  # Выбор заметки для обновления
+    note_index = choice_note(notes)
     if note_index is not None:
-        # Обновление заметки
         updated_note = update_note(notes[note_index])
-        # Замена старой заметки на обновленную
         notes[note_index] = updated_note
         print(Fore.GREEN + "Заметка обновлена:" + Style.RESET_ALL)
-        # Отображение обновленной заметки
         display_note(updated_note, note_index + 1)
 
 
+# Запрашивает у пользователя критерии поиска и отображает найденные заметки.
+# Если заметки не найдены, выводит соответствующее сообщение.
 def search_notes_menu():
-    found_notes = search_notes(notes)  # Поиск заметок
+    found_notes = search_notes(notes)
     if found_notes:
         print(Fore.GREEN + "\nНайденные заметки:" + Style.RESET_ALL)
-        # Отображение найденных заметок
         for i, note in enumerate(found_notes, start=1):
             display_note(note, i)
     else:
-        print(Fore.RED + "Заметки, соответствующие запросу, \
-не найдены." + Style.RESET_ALL)  # Сообщение о отсутствии найденных заметок
+        print(
+            Fore.RED + "Заметки, соответствующие запросу, не найдены." +
+            Style.RESET_ALL
+        )
 
 
-# Функция для отображения меню и обработки выбора пользователя
+# Функция для отображения меню и обработки выбора пользователя.
+# Вызывает соответствующие функции в зависимости от выбора.
 def menu(notes):
-    actions = {
-        '1': create_new_note,  # Создание новой заметки
-        '2': lambda: display_notes(notes),  # Отображение всех заметок
-        '3': update_existing_note,  # Обновление заметки
-        '4': lambda: delete_note(notes),  # удаление заметки
-        '5': search_notes_menu,  # поиск заметок
-        '6': lambda: print(Fore.GREEN + "Программа завершена. Спасибо за \
-использование!" + Style.RESET_ALL) or exit()  # Выход из программы
-    }
-
     while True:
         print(Fore.CYAN + "\nМеню действий:" + Style.RESET_ALL)
         print("1. Создать новую заметку")
@@ -111,33 +97,51 @@ def menu(notes):
         print("5. Найти заметки")
         print("6. Выйти из программы")
 
-        # Получение выбора пользователя
         choice = input(Fore.YELLOW + "Ваш выбор: " + Style.RESET_ALL)
-        # Получение соответствующего действия
-        action = actions.get(choice, lambda: print(Fore.RED + "Неверный \
-выбор. Пожалуйста, выберите действие из списка." + Style.RESET_ALL))
-        action()  # Выполнение действия
+        if choice == '1':
+            create_new_note()
+        elif choice == '2':
+            display_notes(notes)
+        elif choice == '3':
+            update_existing_note()
+        elif choice == '4':
+            delete_note(notes)
+        elif choice == '5':
+            search_notes_menu()
+        elif choice == '6':
+            print(
+                Fore.GREEN +
+                "Программа завершена. Спасибо за использование!" +
+                Style.RESET_ALL
+            )
+            break
+        else:
+            print(
+                Fore.RED +
+                "Неверный выбор. Пожалуйста, выберите действие из списка." +
+                Style.RESET_ALL
+            )
 
 
 # Основная функция для запуска программы
 def main(notes):
-    print(Fore.CYAN + "Добро пожаловать в 'Менеджер \
-заметок'!" + Style.RESET_ALL)
-    menu(notes)  # Вызов функции меню
+    print(
+        Fore.CYAN + "Добро пожаловать в 'Менеджер заметок'!" + Style.RESET_ALL
+    )
+    menu(notes)
 
 
 # Точка входа
 if __name__ == "__main__":
-
     # Список заметок
     notes = [
         {
-            'username': 'Алексей',  # Имя пользователя
-            'titles': ['Список покупок'],  # Заголовок заметки
-            'content': 'Купить продукты на неделю',  # Содержимое заметки
-            'status': 'новая',  # Статус заметки
-            'created_date': '12-01-2025',  # Дата создания заметки
-            'issue_date': '13-02-2025'  # Дата завершения заметки
+            'username': 'Алексей',
+            'titles': ['Список покупок'],
+            'content': 'Купить продукты на неделю',
+            'status': 'новая',
+            'created_date': '12-01-2025',
+            'issue_date': '13-02-2025'
         },
         {
             'username': 'Мария',
